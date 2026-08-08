43349452efae00f992a3ce47b9fe41eaa93d1f5c
Baliram Horgil <baliramhorgil98@gmail.com>
2026-08-08 21:58:55 +0530

    Added Map Feature

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,4 +1,7 @@
 const Listing=require("../Models/listing");
+const mbxGeocoding= require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken=process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index=async (req, res) => {
   const allListings = await Listing.find({});
@@ -28,6 +31,12 @@ module.exports.showListing=async (req, res) => {
 }
 
 module.exports.createListing=async (req, res, next) => {
+ let response=await geocodingClient.forwardGeocode({
+  query: req.body.listing.location,
+  limit: 1, //Default limit is 5 if not set
+})
+  .send();
+
   let url=req.file.path;
   let filename=req.file.filename;
 
@@ -38,7 +47,11 @@ module.exports.createListing=async (req, res, next) => {
 };
 
   newListing.owner=req.user._id;
-  await newListing.save();
+  newListing.geometry=response.body.features[0].geometry;
+
+  let savedListing=await newListing.save();
+  console.log(savedListing);
+
   req.flash("success", "New listing created!");
   res.redirect("/listings");
 }
---
+++ public/js/map.js
@@ -0,0 +1,17 @@
+    // let mapToken=mapToken;
+    // console.log(mapToken);
+    mapboxgl.accessToken=mapToken;
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        center: listing.geometry.coordinates, // starting position [longitude, latitude]. Note that lat must be set between -90 and 90
+        zoom: 9 // starting zoom
+    });
+
+    // Create a default Marker and add it to the map.
+    const marker = new mapboxgl.Marker({color:'red'})
+        .setLngLat(listing.geometry.coordinates) //this long and lat if of Listing.geometry.coordinates
+        .setPopup(new mapboxgl.Popup({offset: 25}) // add popups
+        .setHTML(
+            `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`))
+        .addTo(map);
